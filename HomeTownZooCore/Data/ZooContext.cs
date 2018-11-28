b38efebf5a3654a917b28167c89cc11eb1304327
caseygruse.222 <caseygruse.222@gmail.com>
2018-11-28 14:58:48 -0800

    Created a basic core project

---
+++ HomeTownZooCore/Data/ZooContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeTownZooCore.Models
+{
+    public class ZooContext : DbContext
+    {
+        public ZooContext (DbContextOptions<ZooContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<HomeTownZooCore.Models.Animal> Animal { get; set; }
+    }
+}
--- HomeTownZooCore/Startup.cs
+++ HomeTownZooCore/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using HomeTownZooCore.Models;
 
 namespace HomeTownZooCore
 {
@@ -33,6 +35,9 @@ namespace HomeTownZooCore
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<ZooContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("ZooContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
