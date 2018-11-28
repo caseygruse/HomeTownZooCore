d476985ae7e75db5d23b651b4186bdcebb231281
caseygruse.222 <caseygruse.222@gmail.com>
2018-11-28 13:41:54 -0800

    Add project files.

---
+++ HomeTownZooCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HomeTownZooCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
